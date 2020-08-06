198a1064c5d61aeaa766bab213decf08c94f1e52
Will Fischer <willfischer11@gmail.com>
2020-08-05 21:13:36 -0500

    Prevent crash when player's character dies outside of lab by making character invincible.

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -29,7 +29,13 @@ function ToTheLab(player_index)
         player.print "invalid operation, player already in the lab."
         return
     end
+    
+    if player.character == nil then
+        player.print "No body, no lab. Them's the rules."
+        return
+    end
 
+    player.character.destructible = false
     playerData.character = player.character
 	player.set_controller({type = defines.controllers.god})
     player.teleport({0, 0}, LabName(player.force))
@@ -74,6 +80,7 @@ function ToTheWorld(player_index)
     player.teleport({0, 0}, playerData.character.surface)
 	player.set_controller({type = defines.controllers.character, character = playerData.character})
     playerData.character = nil
+    player.character.destructible = true
 
     ReturnBlueprintImport(player, blueprint)
 
